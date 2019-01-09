f365f24b2ae2bc2a999adee940ad769bed5f2af4
shinyeal <1670806387@qq.com>
2019-01-09 15:16:51 +0800

    head file

---
+++ common.h
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: commen.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年12月19日 星期三 16时02分26秒
+ ************************************************************************/
+
+#ifndef _COMMEN_H
+#define _COMMEN_H
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
+#include <pthread.h>
+#endif
